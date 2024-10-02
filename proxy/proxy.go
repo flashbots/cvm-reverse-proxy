24ced16fe1a3900d54caa07d203ff0e75c97c666
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2024-10-02 11:08:26 +0200

    Handles error cases better

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -2,10 +2,10 @@ package proxy
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 )
 
 type Proxy struct {
@@ -30,9 +30,21 @@ func (p *Proxy) WithTransport(transport *http.Transport) *Proxy {
 }
 
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
 	r.Host = p.target.Host
+
+	if r.Header.Get("X-Forwarded-For") != "" {
+		http.Error(w, "unexpected X-Forwarded-For header passed", http.StatusForbidden)
+		return
+	}
+
 	r.Header.Set("X-Forwarded-For", r.RemoteAddr) // Note: not very reliable. Maybe we should set and check X-Real-IP. Also, X-Forwarded-For might have already been set.
+
+	for headerKey := range r.Header {
+		if strings.HasPrefix("x-flashbots-cert-extensions", strings.ToLower(headerKey)) {
+			http.Error(w, "unexpected X-Flashbots-Cert-Extensions header passed", http.StatusForbidden)
+			return
+		}
+	}
 
 	for _, cert := range r.TLS.PeerCertificates {
 		for _, ext := range cert.Extensions {
@@ -44,14 +56,14 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 			measurements, err := ExtractMeasurementsFromExtension(&ext, atlsVariant)
 			if err != nil {
-				// TODO: log this error!
-				continue // TODO: should we just terminate with 400?
+				http.Error(w, "could not extract measurement from tls extension", http.StatusTeapot)
+				return
 			}
 
 			marshaledPcrs, err := json.Marshal(measurements)
 			if err != nil {
-				// TODO: log this error!
-				continue // TODO: should we just terminate with 400?
+				http.Error(w, "could not marshal measurement extracted from tls extension", http.StatusInternalServerError)
+				return
 			}
 
 			r.Header.Set("X-Flashbots-Cert-Extensions-"+ext.Id.String(), string(marshaledPcrs))
