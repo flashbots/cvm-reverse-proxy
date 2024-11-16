4c15dbf2e2c6ae7189e86d8368a61aad936beff9
Chris Hager <chris@linuxuser.at>
2024-11-16 22:03:24 +0100

    docs

--- common/expected_measurements.go
+++ common/expected_measurements.go
@@ -1,10 +1,48 @@
 package common
 
 //
-// Helper to load expected measurements (from a file or URL),
-// and compare provided measurements against them.
+// Helper to load expected measurements from a file or URL, and compare
+// provided measurements against them.
 //
-// Expected measurements format: https://measurements.builder.flashbots.net
+// Internally this uses the measurements data schema v2 (as served by
+// https://measurements.builder.flashbots.net), but is also backwards
+// compatible with v1 (i.e. measurements.json used by cvm-proxy).
+//
+// Data schema v2:
+//
+//   [
+//       {
+//           "measurement_id": "cvm-image-azure-tdx.rootfs-20241107200854.wic.vhd",
+//           "attestation_type": "azure-tdx",
+//           "measurements": {
+//               "4": {
+//                   "expected": "1b8cd655f5ebdf50bedabfb5db6b896a0a7c56de54f318103a2de1e7cea57b6b"
+//               },
+//               ...
+//           }
+//       },
+//       ...
+//   ]
+//
+// Data schema v1:
+//
+//   {
+//       "azure-tdx-example": {
+//           "15": {
+//               "expected": "0000000000000000000000000000000000000000000000000000000000000000"
+//           },
+//   		...
+//       },
+//       "dcap-tdx-example": {
+//           "4": {
+//               "expected": "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+//           },
+//   		...
+//       }
+//   }
+//
+// The v2 data schema is an improvement because the structure enables
+// additional data fields besides the raw measurements.
 //
 
 import (
@@ -31,6 +69,8 @@ type MeasurementsContainer struct {
 	Measurements    map[string]MeasurementEntry `json:"measurements"`
 }
 
+// NewExpectedMeasurementsFromFile returns an ExpectedMeasurements instance,
+// with the measurements loaded from a file or URL.
 func NewExpectedMeasurementsFromFile(path string) (m *ExpectedMeasurements, err error) {
 	var data []byte
 	if strings.HasPrefix(path, "http") {
@@ -40,7 +80,6 @@ func NewExpectedMeasurementsFromFile(path string) (m *ExpectedMeasurements, err 
 			return nil, err
 		}
 		defer resp.Body.Close()
-
 		data, err = io.ReadAll(resp.Body)
 		if err != nil {
 			return nil, err
@@ -53,16 +92,14 @@ func NewExpectedMeasurementsFromFile(path string) (m *ExpectedMeasurements, err 
 		}
 	}
 
-	m = &ExpectedMeasurements{
-		Measurements: make([]MeasurementsContainer, 0),
-	}
+	m = &ExpectedMeasurements{}
 	err = json.Unmarshal(data, &m.Measurements)
 	return m, err
 }
 
 // Contains checks if the provided measurements match one of the known measurements. Any keys in the provided
 // measurements which are not in the known measurements are ignored.
-func (m *ExpectedMeasurements) Contains(measurements map[string]string) (bool, string) {
+func (m *ExpectedMeasurements) Contains(measurements map[string]string) (found bool, measurementID string) {
 	// For every known container, all known measurements match (and additional ones are ignored)
 	for _, container := range m.Measurements {
 		allMatch := true
